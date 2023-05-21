4d363b688d82231fe0f3712176256ce580801381
Romina M <martinezrominapaola@gmail.com>
2023-05-21 03:18:23 -0300

    segunda pre-entrega

--- js/codigo.js
+++ js/codigo.js
@@ -1,5 +1,45 @@
 window.onload = function(){
-    $('#onload').fadeOut();
-    $('#nav').addClass('fixed');
+    // $('#onload').fadeOut();
+    // $('#nav').addClass('fixed');
     $('body').removeClass('hidden');
+    $('#navBar').removeClass('hidden');
+
 }
+
+let theEnd =0,
+    navbar = document.getElementById('navBar');
+
+window.addEventListener('scroll', function(){
+    var scrollTop = window.pageXOffset || document.
+    documentElement.scrollTop;
+    if(scrollTop > theEnd){
+        navbar.style.top = '-70px';
+    }
+    else{
+        navbar.style.top = '0'
+    }
+    theEnd = scrollTop;
+})
+
+var loader;
+function loadNow(opacity){
+    if(opacity <=0){
+        displayContent();
+    }
+    else{
+        loader.style.opacity = opacity;
+        window.setTimeout(function(){
+            loadNow(opacity - 0.05)
+        },100);
+    }
+}
+
+function displayContent(){
+    loader.style.display = 'none';
+    Document.getElementById('content').style.display = 'block';
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    loader = document.getElementById('loader');
+    loadNow(1);
+})
